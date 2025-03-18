03271dc290196f6b150a327e130eff34148c2b54
Mostafa-alsheikh <M.Mohamed2262@nu.edu.eg>
2025-03-18 17:42:45 +0200

    Added 2 more visualizations

--- bd-a1/vis.py
+++ bd-a1/vis.py
@@ -6,15 +6,31 @@ import os
 
 df = pd.read_csv("res_dpre.csv")
 
-sns.set_style("whitegrid")
+gender_count = df["Sex"].value_counts()
 plt.figure(figsize=(6, 4))
+gender_count.plot(kind="bar", color=["blue", "pink"])
+plt.title("Passenger Count by Gender")
+plt.xlabel("Gender")
+plt.ylabel("Count")
+plt.savefig("vis-1-gender.png")
+plt.close()
 
-sns.countplot(x="Survived", data=df, palette="viridis")
-plt.xlabel("Survival Status")
-plt.ylabel("Passenger Count")
+survival_count = df["Survived"].value_counts()
+plt.figure(figsize=(6, 6))
+survival_count.plot(kind="pie", autopct="%1.1f%%", colors=["red", "green"])
 plt.title("Survivors vs Non-Survivors")
+plt.ylabel("")  
+plt.savefig("vis-2-survival.png")
+plt.close()
+
+plt.figure(figsize=(6, 4))
+df["Age"].plot(kind="hist", bins=20, color="purple", edgecolor="black")
+plt.title("Age Distribution")
+plt.xlabel("Age")
+plt.ylabel("Frequency")
+plt.savefig("vis-3-age.png")
+plt.close()
 
 
-plt.savefig("vis.png")
 os.system("python3 model.py")
-print("Visualization saved as vis.png")
+print("Visualizations saved")
